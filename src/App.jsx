1cdf45e0aaa00adb527c8e44ae01d30a5ef4c41a
DowonKim <117058112+KoreaMoney@users.noreply.github.com>
2023-05-16 14:25:26 +0900

    components추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Keyboard from "./components/Keyboard";
 
 const App = () => {
-  return <div>여기는 캐러셀 시작하는 구간입니다</div>;
+  return (
+    <div>
+      여기는 캐러셀 시작하는 구간입니다
+      <Keyboard />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Keyboard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Keyboard = () => {
+  return <div>Keyboard 이건 컴포넌트입니다</div>;
+};
+
+export default Keyboard;
