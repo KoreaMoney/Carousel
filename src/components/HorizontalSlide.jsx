0bc58c2f24a25bd09c7ed696d1dfa5b439b2bfb4
DowonKim <kimdowon1404@gmail.com>
2023-05-20 11:28:02 +0900

    yarn설치 error해결

--- src/components/HorizontalSlide.jsx
+++ src/components/HorizontalSlide.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import { Carousel } from "@mantine/carousel";
 import "../styles/horizontalSlide.css";
 
 const HorizontalSlide = () => {
-  return <div>가로 슬라이드</div>;
+  return (
+    <div>
+      가로 슬라이드<Carousel></Carousel>
+    </div>
+  );
 };
 
 export default HorizontalSlide;
