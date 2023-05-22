d561b5ece543f5ac4155d01a7c9a8e0d561eeb3f
KIMDOWON <dw.kim@theinfoin.com>
2023-05-22 20:03:11 +0900

    스크롤 변경으로 인한 이미지 추가

--- src/components/HorizontalSlide.jsx
+++ src/components/HorizontalSlide.jsx
@@ -13,8 +13,9 @@ import pic6 from "../assets/pic6.webp";
 import pic7 from "../assets/pic7.webp";
 import pic8 from "../assets/pic8.webp";
 import pic9 from "../assets/pic9.webp";
+import pic10 from "../assets/pic10.webp";
 
-const images = [pic2, pic1, pic3, pic4, pic5, pic6, pic7, pic8, pic9];
+const images = [pic2, pic1, pic3, pic4, pic5, pic6, pic7, pic8, pic9, pic10];
 
 const HorizontalSlide = () => {
   //[State관리]
@@ -58,11 +59,19 @@ const HorizontalSlide = () => {
   return (
     <div>
       <Carousel
-        controlSize={35}
-        sx={{ maxWidth: 500, margin: "0 auto" }}
-        mx="auto"
+        controlSize={40}
+        sx={{ maxWidth: 1480, margin: "0 auto" }}
+        slideSize="50.8%"
+        slideGap="md"
+        loop
+        align="start"
         withIndicators
-        height={300}
+        slidesToScroll={2}
+        height={500}
+        breakpoints={[
+          { maxWidth: "md", slideSize: "50%" },
+          { maxWidth: "sm", slideSize: "100%", slideGap: 0 },
+        ]}
         getEmblaApi={setSlideCarousel}
         styles={{
           indicator: {
@@ -74,6 +83,7 @@ const HorizontalSlide = () => {
               width: rem(50),
             },
             backgroundColor: "#ffffff",
+            marginBottom: "30px",
           },
           control: {
             "&[data-inactive]": {
@@ -87,10 +97,10 @@ const HorizontalSlide = () => {
       </Carousel>
       <div className="button">
         <Button color="gray" radius="md" onClick={handlePrevSlide}>
-          이전
+          Prev
         </Button>
         <Button color="gray" radius="md" onClick={handleNextSlide}>
-          다음
+          Next
         </Button>
       </div>
     </div>
